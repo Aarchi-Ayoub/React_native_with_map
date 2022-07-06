8c233e868950818e9ed086bcf69cd2eb1d6177d3
Aarchi-Ayoub <ayoubaa084@gmail.com>
2022-07-06 18:25:23 +0100

    zoom to user position

--- src/containers/Positions/index.js
+++ src/containers/Positions/index.js
@@ -1,15 +1,16 @@
-import React, {useRef, useState} from 'react';
-import {View} from 'react-native';
+import React, {useRef, useState, useEffect} from 'react';
+import {Alert, Image, Linking, TouchableOpacity, View} from 'react-native';
 import MapView, {Marker} from 'react-native-maps';
-
+import Geolocation from '@react-native-community/geolocation';
 // styles
 import styles from './styles';
 // config
 import {MAP_KEY} from 'react-native-dotenv';
 import MapViewDirections from 'react-native-maps-directions';
 export default () => {
-  //
+  // Map ref
   const mapRef = useRef();
+
   // local states
   const [initialRegion, setInitialRegion] = useState({
     latitude: 34.06446226508741,
@@ -28,6 +29,66 @@ export default () => {
     longitude: -6.771591746405573,
   });
 
+  const [position, setPosition] = useState({
+    latitude: 0,
+    longitude: 0,
+    latitudeDelta: 0,
+    longitudeDelta: 0,
+  });
+
+  // Life cycle
+  useEffect(() => {
+    Geolocation.getCurrentPosition(pos => {
+      const {coords} = pos;
+      setPosition({
+        latitude: coords.latitude,
+        longitude: coords.longitude,
+        latitudeDelta: 0.0421,
+        longitudeDelta: 0.0421,
+      });
+      console.log(pos);
+    }).catch(err => {
+      console.log(err);
+    });
+  }, []);
+
+  // Get my position
+  const myPosition = () => {
+    Geolocation.getCurrentPosition(pos => {
+      // Extract my location infos
+      const {coords} = pos;
+      const {latitude, longitude} = coords;
+      const DEFAULT_PADDING = {top: 40, right: 40, bottom: 40, left: 40};
+      mapRef.current.fitToCoordinates([{latitude, longitude}], {
+        edegPadding: DEFAULT_PADDING,
+        animated: true,
+      });
+    }).catch(err => {
+      console.log('Get Current Position Error: ', err);
+    });
+  };
+
+  // Show the two markes in the page center
+  const markersFocus = result => {
+    mapRef.current.fitToCoordinates(result.coordinates, {
+      edegPadding: {
+        top: 100,
+        right: 30,
+        left: 30,
+        bottom: 300,
+      },
+    });
+  };
+
+  // Help desk call
+  const infoCall = () => {
+    try {
+      Linking.openURL('tel:911');
+    } catch (error) {
+      console.log('Can Open URL Error : ', error);
+      Alert.alert(error);
+    }
+  };
   return (
     <View style={styles.containers}>
       <MapView
@@ -39,6 +100,8 @@ export default () => {
         userLocationPriority={'balanced'}>
         <Marker coordinate={destination} />
         <Marker coordinate={depart} />
+        <Marker coordinate={position} />
+
         <MapViewDirections
           apikey={MAP_KEY}
           origin={depart}
@@ -47,18 +110,25 @@ export default () => {
           strokeWidth={5}
           optimizeWaypoints={true}
           mode={'DRIVING'}
-          onReady={result => {
-            mapRef.current.fitToCoordinates(result.coordinates, {
-              edegPadding: {
-                top: 100,
-                right: 30,
-                left: 30,
-                bottom: 300,
-              },
-            });
-          }}
+          onReady={result => markersFocus(result)}
         />
       </MapView>
+      <View style={styles.btnRow}>
+        <TouchableOpacity onPress={infoCall} style={styles.info}>
+          <Image
+            style={styles.btnImg}
+            source={require('assets/infos.png')}
+            resizeMode="contain"
+          />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={myPosition} style={styles.myPosition}>
+          <Image
+            style={styles.btnImg}
+            source={require('assets/myLocation.png')}
+            resizeMode="contain"
+          />
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
--- src/containers/Positions/styles.js
+++ src/containers/Positions/styles.js
@@ -4,4 +4,31 @@ export default StyleSheet.create({
   containers: {
     flex: 1,
   },
+
+  myPosition: {
+    position: 'absolute',
+    backgroundColor: 'white',
+    bottom: 50,
+    right: 20,
+    height: 50,
+    width: 50,
+    borderRadius: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  info: {
+    position: 'absolute',
+    backgroundColor: 'white',
+    bottom: 50,
+    left: 20,
+    height: 50,
+    width: 50,
+    borderRadius: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  btnImg: {
+    height: 30,
+    width: 30,
+  },
 });
