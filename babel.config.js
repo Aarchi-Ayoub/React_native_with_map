4f0402ad1705e5972151f245d4389a4cbd91eb9e
Aarchi-Ayoub <ayoubaa084@gmail.com>
2022-07-06 15:48:47 +0100

    babel config

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,16 @@
-module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    // presets: ['babel-preset-expo'],
+    presets: ['module:metro-react-native-babel-preset'],
+
+    plugins: [
+      [
+        'module:react-native-dotenv',
+        {
+          moduleName: 'react-native-dotenv',
+        },
+      ],
+    ],
+  };
 };
