532db1b96a312c5959d8e4217a1f1e0e3691ff90
Aarchi-Ayoub <ayoubaa084@gmail.com>
2022-09-03 12:34:26 +0100

    styles file

---
+++ src/containers/Positions/BottomSheet /styles.js
@@ -0,0 +1,18 @@
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: '#fff',
+  },
+  contentContainer: {
+    alignSelf: 'flex-end',
+    paddingHorizontal: 5,
+  },
+  closeContainer: {},
+  closeImg: {
+    height: 35,
+    width: 35,
+  },
+});
