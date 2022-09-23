cbf0694346e37f50905f976a86f07d5bf82c86fd
Aarchi-Ayoub <ayoubaa084@gmail.com>
2022-09-23 14:47:55 +0100

    change the adress globaly

--- src/containers/Localisation/index.js
+++ src/containers/Localisation/index.js
@@ -1,26 +1,57 @@
 import React, {useState, useEffect} from 'react';
 import {View, ScrollView, TouchableOpacity, Text} from 'react-native';
+import {useDispatch} from 'react-redux';
+import {useNavigation} from '@react-navigation/native';
+// Comp
 import PickAdress from './PickAdress';
+// Actions
+import {setStart, setEnd} from 'slices/DestinationSlice';
+// Styles
 import styles from './styles';
 
 export default () => {
+  // States
   const [disabled, setDisabled] = useState(false);
   const [startPosition, setStartPosition] = useState(null);
   const [endPosition, setEndPosition] = useState(null);
 
+  // Hooks
+  const dispatch = useDispatch();
+  const navigation = useNavigation();
+
+  // Life Cycle
   useEffect(() => {
-    if (!startPosition && !endPosition) {
+    if (!startPosition || !endPosition) {
       setDisabled(true);
     } else {
       setDisabled(false);
     }
   }, [startPosition, endPosition]);
 
+  /** Set positions */
   const startAdress = arg => {
-    console.log('startAdress', arg);
+    setStartPosition(arg);
+    dispatch(
+      setStart({
+        latitude: arg?.lat,
+        longitude: arg?.lng,
+      }),
+    );
   };
-  const endAdress = () => {
-    console.log('endAdress');
+  const endAdress = arg => {
+    setEndPosition(arg);
+    dispatch(
+      setEnd({
+        latitude: arg?.lat,
+        longitude: arg?.lng,
+      }),
+    );
+  };
+  /** Set positions */
+
+  // Back to maps view
+  const confirm = () => {
+    navigation.goBack();
   };
   return (
     <View style={styles.container}>
@@ -37,6 +68,7 @@ export default () => {
         />
         <TouchableOpacity
           disabled={disabled}
+          onPress={confirm}
           style={disabled ? [styles.btn, styles.disabled] : styles.btn}>
           <Text style={styles.btnText}>Confirmer</Text>
         </TouchableOpacity>
